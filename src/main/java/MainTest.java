bcba069e3b2f074686652cb402197068160ff51e
clarkrao <clarkrao25@gmail.com>
2019-04-14 16:32:11 +0800

    Test

---
+++ src/main/java/MainTest.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: ClarkRao
+ * @Date: 2019/4/14 16:30
+ * @Description: 主要测试主程序
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println("-------Test-------");
+    }
+}
