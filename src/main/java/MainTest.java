f70b569e88716d8a5e91d30c0dcf23feacef2f9e
clarkrao <clarkrao25@gmail.com>
2019-04-16 23:29:35 +0800

    配置log

--- src/main/java/MainTest.java
+++ src/main/java/MainTest.java
@@ -1,10 +1,13 @@
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @Author: ClarkRao
  * @Date: 2019/4/14 16:30
  * @Description: 主要测试主程序
  */
+@Slf4j
 public class MainTest {
     public static void main(String[] args) {
-        System.out.println("-------Test-------");
+       log.info("-------Test-------");
     }
 }
